5ace0bad472b3ac911cabb5cdff6e3aefe906a7c
tracy-u <tracy.allen.urquhart@gmail.com>
2021-10-21 10:28:29 +0000

    update customers config to table

---
+++ coffee-shop-dbt/models/example/customers.sql
@@ -0,0 +1,19 @@
+
+{{ config(materialized='view') }}
+
+WITH orders AS (
+    SELECT customer_id
+    , MIN(created_at) first_order_at
+    , COUNT(DISTINCT id) number_of_orders
+    FROM analytics-engineers-club.coffee_shop.orders
+    GROUP BY 1
+)
+
+SELECT o.customer_id
+, c.name
+, c.email
+, o.first_order_at
+, o.number_of_orders
+FROM analytics-engineers-club.coffee_shop.customers c
+LEFT JOIN orders o ON o.customer_id=c.id
+ORDER BY first_order_at asc
