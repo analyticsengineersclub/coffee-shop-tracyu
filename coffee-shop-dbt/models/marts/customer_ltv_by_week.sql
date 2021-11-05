50e9e413117456ebbb28673c9ae801e3b9ba7392
tracy-u <tracy.allen.urquhart@gmail.com>
2021-11-05 09:24:32 +0000

    add comments, clean up and format

--- coffee-shop-dbt/models/marts/customer_ltv_by_week.sql
+++ coffee-shop-dbt/models/marts/customer_ltv_by_week.sql
@@ -1,5 +1,7 @@
 {{ config(materialized='table') }}
 
+-- for each order_items id, determine the week of the order
+-- and the week the customer was acquired
 with weekly_revenue as (
     select *
     , cast(date_trunc(order_created_at, week) as date) as order_week
@@ -9,6 +11,8 @@ with weekly_revenue as (
     from {{ ref('revenue')}}
 )
 
+-- for each customer_id, determine the week in their cusomter lifetime
+-- with the first week starting at 1; total revenue for each customer and week
 , weeks_counted as (
     select customer_id
     , date_diff(order_week, week_acquired, week)+1 as week_count
@@ -18,20 +22,26 @@ with weekly_revenue as (
     order by customer_id, week_count desc
 )
 
+-- for each customer, determine the max number of weeks 
+-- and generate a count from to max number of weeks 
 , max_weeks as ( 
     select *
-    from (select customer_id, max(week_count) as max_week_count
-            FROM weeks_counted
-            group by 1
-            ) wc join
-            unnest(generate_array(1, wc.max_week_count)) week_num
+    from (select customer_id
+                , max(week_count) as max_week_count
+         from weeks_counted
+         group by 1
+         ) wc 
+    join unnest(generate_array(1, wc.max_week_count)) as week_num
 )
 
+-- for each customer, list id, week, revenue for the week
+-- and cumulative revenue
 select max_weeks.customer_id
 , max_weeks.week_num as week
 , coalesce(revenue, 0) as revenue
-, sum(revenue) over (partition by max_weeks.customer_id order by week_num) as cumulative_revenue
+, sum(coalesce(revenue, 0)) over (partition by max_weeks.customer_id order by week_num) as cumulative_revenue
 from max_weeks
 left join weeks_counted 
     on max_weeks.customer_id=weeks_counted.customer_id
-    and weeks_counted.week_count=max_weeks.week_num
+    and max_weeks.week_num=weeks_counted.week_count
+order by customer_id, week
