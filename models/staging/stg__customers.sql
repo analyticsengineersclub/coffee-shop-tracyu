d137968aa2ab2c1e0ffec553be49bfe0044525b7
tracy-u <tracy.allen.urquhart@gmail.com>
2021-11-17 01:52:52 +0000

    update stg__customers model with codegen

--- models/staging/stg__customers.sql
+++ models/staging/stg__customers.sql
@@ -1,5 +1,21 @@
 
 {{ config(materialized='table') }}
 
-select *
-from {{ source('coffee_shop', 'customers') }}
+with source as (
+
+    select * from {{ source('coffee_shop', 'customers') }}
+
+),
+
+renamed as (
+
+    select
+        id,
+        name,
+        email
+
+    from source
+
+)
+
+select * from renamed
